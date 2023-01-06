210a81ae3229a3e380f119d710be3f9957e0ce16
Kdc693 <122105152+Kdc693@users.noreply.github.com>
2023-01-06 22:42:18 +0800

    Initial commit

---
+++ to-do-list/src/App.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from 'react';
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Todolist from './components/Todolist';
+import './App.css';
+
+const App = () => {
+  
+  const initialState = JSON.parse(localStorage.getItem("tasks")) || [];
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState(initialState);
+  const [editTask, setEditTask] = useState(null);
+
+  useEffect (() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form 
+          input={input}
+          setInput={setInput}
+          tasks={tasks}
+          setTasks={setTasks}
+          editTask={editTask}
+          setEditTask={setEditTask}
+          />
+        </div>
+        <div>
+          <Todolist tasks={tasks} setTasks={setTasks} setEditTask={setEditTask}/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
